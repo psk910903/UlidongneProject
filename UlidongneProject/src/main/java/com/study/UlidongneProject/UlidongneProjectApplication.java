cb1f6b33ea01c0122ba16d8f1b1bffa155bcf78e
MakingNameIsAnnoying <121778872+MakingNameIsAnnoying@users.noreply.github.com>
2023-03-24 18:12:18 +0900

    엔티티 작성, sql수정, gradle수정

--- UlidongneProject/src/main/java/com/study/UlidongneProject/UlidongneProjectApplication.java
+++ UlidongneProject/src/main/java/com/study/UlidongneProject/UlidongneProjectApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class UlidongneProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UlidongneProjectApplication.class, args);
 	}
-
 }
