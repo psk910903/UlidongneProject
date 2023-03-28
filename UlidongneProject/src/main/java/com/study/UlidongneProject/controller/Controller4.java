fef41e48fa08b4e4f475c702d6ebbef2d7bafc24
Heejinee3 <102013918+Heejinee3@users.noreply.github.com>
2023-03-28 16:29:09 +0900

    수정 0328 16:28

--- UlidongneProject/src/main/java/com/study/UlidongneProject/controller/Controller4.java
+++ UlidongneProject/src/main/java/com/study/UlidongneProject/controller/Controller4.java
@@ -1,7 +1,7 @@
 package com.study.UlidongneProject.controller;
 
 import com.study.UlidongneProject.entity.MemberEntity;
-import com.study.UlidongneProject.entity.MemberRepository;
+//import com.study.UlidongneProject.entity.MemberRepository;
 import com.study.UlidongneProject.service.Service3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
-public class Controller5 {
-
-    private final Service3 service3;
-    private final MemberRepository memberRepository;
-    @GetMapping("/testheejin")
-    public String testheejin(Model model) {
-
-        return "/testheejin";
-    }
-
-    @ResponseBody
-    @GetMapping("/boards")
-    public MemberEntity findAll(){
-        MemberEntity memberEntity = service3.findById("01012345678");
-        return memberEntity;
-    }
 public class Controller4 {
+
+//    private final Service3 service3;
+//    private final MemberRepository memberRepository;
+//
+//    @GetMapping("/testheejin")
+//    public String testheejin(Model model) {
+//
+//        return "/testheejin";
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/boards")
+//    public MemberEntity findAll() {
+//        MemberEntity memberEntity = service3.findById("01012345678");
+//        return memberEntity;
+//    }
 }
