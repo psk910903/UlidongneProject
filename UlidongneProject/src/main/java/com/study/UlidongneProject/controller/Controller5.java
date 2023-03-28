fb19c93ac125e263e66fd094278df340401f28fb
Heejinee3 <102013918+Heejinee3@users.noreply.github.com>
2023-03-28 16:25:27 +0900

    수정 0328 16:25

--- UlidongneProject/src/main/java/com/study/UlidongneProject/controller/Controller5.java
+++ UlidongneProject/src/main/java/com/study/UlidongneProject/controller/Controller5.java
@@ -1,7 +1,31 @@
 package com.study.UlidongneProject.controller;
 
+import com.study.UlidongneProject.entity.MemberEntity;
+import com.study.UlidongneProject.entity.MemberRepository;
+import com.study.UlidongneProject.service.Service3;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@RequiredArgsConstructor
 @Controller
 public class Controller5 {
+
+    private final Service3 service3;
+    private final MemberRepository memberRepository;
+    @GetMapping("/testheejin")
+    public String testheejin(Model model) {
+
+        return "/testheejin";
+    }
+
+    @ResponseBody
+    @GetMapping("/boards")
+    public MemberEntity findAll(){
+        MemberEntity memberEntity = service3.findById("01012345678");
+        return memberEntity;
+    }
 }
