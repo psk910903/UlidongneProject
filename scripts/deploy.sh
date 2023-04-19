6d44eaac0b3c9c0781d2912b42c80673eebbe08c
psk910903 <psk910903@gmail.com>
2023-04-19 14:58:03 +0900

    0419

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 #CURRENT_PID=$(pgrep -f $PROJECT_NAME)
-CURRENT_PID=$(pgrep -f ec2-loginjoindb.jar)
+CURRENT_PID=$(pgrep -f UlidongneProject.jar)
 #CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
