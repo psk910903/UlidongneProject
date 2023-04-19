a72bab947b84bfefd4ce914b65242d4c63a1e0e7
psk910903 <psk910903@gmail.com>
2023-04-19 14:36:15 +0900

    0419

--- src/main/java/com/study/UlidongneProject/UlidongneProjectApplication.java
+++ src/main/java/com/study/UlidongneProject/UlidongneProjectApplication.java
@@ -3,11 +3,15 @@ package com.study.UlidongneProject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class UlidongneProjectApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(UlidongneProjectApplication.class, args);
+//		SpringApplication.run(UlidongneProjectApplication.class, args);
+		SpringApplication application = new SpringApplication(UlidongneProjectApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
