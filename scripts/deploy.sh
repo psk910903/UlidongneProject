2d3f5a771ed005b845a0921c18a225e07c311e88
psk910903 <psk910903@gmail.com>
2023-04-19 15:02:13 +0900

    0419

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=UlidongneProject #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
+PROJECT_NAME=ec2-project2 #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 #CURRENT_PID=$(pgrep -f $PROJECT_NAME)
-CURRENT_PID=$(pgrep -f UlidongneProject.jar)
+CURRENT_PID=$(pgrep -f ec2-project2.jar)
 #CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
