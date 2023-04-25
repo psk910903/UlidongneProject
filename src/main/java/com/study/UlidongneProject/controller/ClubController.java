dbe6d56bf1c602d1c822e6b8a286be944dc7a443
MakingNameIsAnnoying <121778872+MakingNameIsAnnoying@users.noreply.github.com>
2023-04-25 11:55:40 +0900

    d
    d

--- src/main/java/com/study/UlidongneProject/controller/ClubController.java
+++ src/main/java/com/study/UlidongneProject/controller/ClubController.java
@@ -29,8 +29,7 @@ public class ClubController {
     private final ChatServiceImpl chatService;
     private final CategoryService categoryService;
     private final AwsS3Service awsS3Service;
-
-
+    
 
     @GetMapping("/club/{param}") // 클럽 정보 조회
     public String clubDetailPage(@PathVariable("param") Long clubIdx, HttpSession session, Model model){
